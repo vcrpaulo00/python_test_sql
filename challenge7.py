982650239252ed3a84599f93bebf808f7e41b154
Paulo Victor Carvalho Rodrigues <paulorodrigues@sdc-sc.intranet.sdc.sc.gov.br>
2025-03-19 17:57:13 -0300

    I am updating my contribution at work in the afternoon

---
+++ challenge7.py
@@ -0,0 +1,9 @@
+def pauloTest ():
+
+    first_name = str(input('Enter your first name: '))
+    last_name = str(input('Enter your last name: '))
+    address = str(input('Enter your address: '))
+    number_phone = int(input('Enter your phone number: '))
+
+
+pauloTest ()
